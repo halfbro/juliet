c3792ccdde5a44979f34d84cebad722c7a64ab64
halfbro <mlagaren@stevens.edu>
2015-10-24 23:52:46 -0400

    Change juliet_module to load before other modules for dependency reasons

--- juliet_importer.py
+++ juliet_importer.py
@@ -3,15 +3,14 @@ import imp
 
 modules = {}
 
-def load_modules(path="./modules/"): # Consider adding recursive sorting at some point in the future
+def load_modules(path="./modules/"): # Consider adding recursive searching at some point in the future
+    modules['juliet_module'] = imp.load_source('juliet_module', path + "juliet_module.py")
     names = os.listdir(path)
     for name in names:
         if not name.endswith(".py"): continue
         print("Importing module {0}".format(name))
-        name = name.split('.')[0]
         try:
-            new_module = imp.load_source(name, path)
-            modules[name] = new_module
+            modules[name.split('.')[0]] = imp.load_source(name.split('.')[0], path + name)
         except ImportError as e:
             print("Error importing module {0} from directory {1}".format(name,os.getcwd()))
             print(e)
