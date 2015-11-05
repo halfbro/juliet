7d8d2e3249b7562a9a705335ef6ef1b55f8345b7
halfbro <mlagaren@stevens.edu>
2015-11-05 13:37:40 -0500

    Update text_module.py to have an initialization string

--- modules/text_module.py
+++ modules/text_module.py
@@ -4,25 +4,27 @@ from pygame import Rect, font, draw
 class text_module(juliet_module.module):
     mod_name = "text_module"
     mod_id = -1
-    mod_size = Rect(0,0,0,0)
+    mod_rect = Rect(0,0,0,0)
+    changed = False
 
     module_font = None
-    text = "Hello World!"
+    text = None
 
-    def __init__(self, _id):
+    def __init__(self, _id, init_text):
         self.mod_id = _id
-        self.mod_size = Rect(0,0,720,100)
-        self.module_font = font.Font("Comme-Thin.ttf", 100)
+        self.text = init_text
+        self.module_font = font.Font("Comme-Thin.ttf", 50)
+        self.mod_rect = Rect((0,0), self.module_font.size(self.text)).inflate(10,0)
 
     def draw(self, surf):
+        "Draw this module to the surface provided. Returns a Rect of the affected area"
         text_surface = self.module_font.render(self.text, True, (255,255,255))
-        dirty = surf.blit(text_surface, (0,-30))
+        dirty = surf.blit(text_surface, (5,0))
         draw.rect(surf, (255,255,255), surf.get_rect(), 4)
         return dirty
 
     def update(self):
-        "Update this module's internal state (do things like time updates, get weather, etc."
-        print("Update call of text module")
+        "This module's text cannot be updated"
 
-def new_module(_id):
-    return text_module(_id)
+def new_module(_id, text = "..."):
+    return text_module(_id, text)
